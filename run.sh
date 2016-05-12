7f0c9a20b54a293f7e1719943696d698a06f059e
Gregory <grihabor@mail.ru>
2016-05-12 17:12:12 +0300

    bash run script

---
+++ run.sh
@@ -0,0 +1,7 @@
+python3 ../neural-doodle-master/doodle.py \
+                --style samples/womangit.jpeg \
+                --content samples/elya.jpg \
+                --output elya_draw.png \
+                --device=cpu \
+                --phases=4 \
+                --iterations=40
